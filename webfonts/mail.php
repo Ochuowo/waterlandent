2ecf14a79c3bdf675580decf6814bea2865ff96d
Ochuo <43129579+Ochuowo@users.noreply.github.com>
2021-11-13 15:32:14 +0300

    Update mail.php
    get data from form

--- webfonts/mail.php
+++ webfonts/mail.php
@@ -1,32 +1,15 @@
-<h2>Test Mail</h2>
 <?php
-
-if (!isset($_POST["submit"]))
-  {
-  ?>
-  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-  From: <input type="text" name="from"><br>
-  Email: <input type="text" name="email"><br>
-  Message: <textarea rows="20" cols="40" name="message"></textarea><br>
-  <input type="submit" name="submit" value="Click To send mail">
-  </form>
-  <?php
-  }
-
-else
-
-  {
-
-  if (isset($_POST["from"]))
-    {
-    $from = $_POST["from"]; // sender
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-
-    $message = wordwrap($message, 70);
-
-    mail("waterlandent@gmail.com",$subject,$message,"From: $from\n");
-    echo "Thanks for your response.";
-    }
-  }
-?>
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "youremail@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+echo "Thanks for your response.";
